2282e8b0edcd32d27b63fb0a1148fb28552ecddf
Joe Lee <jdlee6@buffalo.edu>
2022-08-11 16:31:35 -0400

    Dynamic components

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,38 @@
 <template>
   <div>
-    <!-- pass down html templates as 'slots' -->
-    <!-- 1. define what html to pass down -->
-    <form-helper>
-      <!-- name slots -->
-      <!-- <h2 slot="title">{{ title }}</h2> -->
-      <!-- <p slot="text">I am the slot description</p> -->
-      <div slot="form-header">
-        <h3>This is the title of the form</h3>
-      </div>
-      <div slot="form-fields">
-        <input type="text" placeholder="name" />
-        <input type="password" placeholder="password" password />
-      </div>
+    <!-- <form-one></form-one> -->
+    <!-- <form-two></form-two> -->
 
-      <div slot="form-controls">
-        <button v-on:click="handleSubmit">Submit</button>
-      </div>
-    </form-helper>
+    <!-- keep-alive tag to retain state within component -->
+    <!-- Vue tag to display dynamic components -->
+    <keep-alive>
+      <component v-bind:is="component"></component>
+    </keep-alive>
+    <button v-on:click="component = 'form-one'">Show Form 1</button>
+    <button v-on:click="component = 'form-two'">Show Form 2</button>
   </div>
 </template>
 
 <script>
-import formHelper from './components/FormHelper.vue';
+// import formHelper from './components/FormHelper.vue';
+import formOne from './components/formOne.vue';
+import formTwo from './components/formTwo.vue';
 
 export default {
   components: {
-    'form-helper': formHelper
+    // 'form-helper': formHelper
+    'form-one': formOne,
+    'form-two': formTwo
   },
   data() {
     return {
-      title: 'I am dynamic slot title'
+      // title: 'I am dynamic slot title'
+      // We can dynamically change the component 
+      // by changing this state here
+      component: 'form-one'
     }
   },
   methods: {
-    handleSubmit() {
-      console.log('submitted!')
-    }
   },
 }
 </script>
