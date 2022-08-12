cc96f7a2663a04b05cc884a338701467bb2c6454
Joe Lee <jdlee6@buffalo.edu>
2022-08-12 13:52:41 -0400

    Input binding

--- src/App.vue
+++ src/App.vue
@@ -5,31 +5,35 @@
 
     <!-- keep-alive tag to retain state within component -->
     <!-- Vue tag to display dynamic components -->
-    <keep-alive>
-      <component v-bind:is="component"></component>
-    </keep-alive>
-    <button v-on:click="component = 'form-one'">Show Form 1</button>
-    <button v-on:click="component = 'form-two'">Show Form 2</button>
+    <!-- <keep-alive> -->
+    <!-- <component v-bind:is="component"></component> -->
+    <!-- </keep-alive> -->
+    <!-- <button v-on:click="component = 'form-one'">Show Form 1</button> -->
+    <!-- <button v-on:click="component = 'form-two'">Show Form 2</button> -->
+
+    <add-blog></add-blog>
   </div>
 </template>
 
 <script>
 // import formHelper from './components/FormHelper.vue';
-import formOne from './components/formOne.vue';
-import formTwo from './components/formTwo.vue';
+// import formOne from './components/formOne.vue';
+// import formTwo from './components/formTwo.vue';
+import addBlog from './components/addBlog.vue';
 
 export default {
   components: {
+    'add-blog': addBlog
     // 'form-helper': formHelper
-    'form-one': formOne,
-    'form-two': formTwo
+    // 'form-one': formOne,
+    // 'form-two': formTwo
   },
   data() {
     return {
       // title: 'I am dynamic slot title'
       // We can dynamically change the component 
       // by changing this state here
-      component: 'form-one'
+      // component: 'form-one'
     }
   },
   methods: {
