08b6937b3a5a5982580c6529cf5ad7335c85e737
Joe Lee <jdlee6@buffalo.edu>
2022-09-25 19:05:24 -0400

    Custom directives

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -1,8 +1,10 @@
 <template>
-  <div id="show-blogs">
+  <!-- directives & arguments -->
+  <div v-theme:column="'narrow'" id="show-blogs">
     <h1>All Blog Articles</h1>
     <div v-for="blog in blogs" class="single-blog">
-      {{ blog.title }}
+      <h2 v-rainbow>{{ blog.title }}</h2>
+      <article>{{ blog.body }}</article>
     </div>
   </div>
 </template>
