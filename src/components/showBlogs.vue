eacc3069c4aced9bf13c93eceda286e5f32c44c0
Joe Lee <jdlee6@buffalo.edu>
2022-09-24 23:07:49 -0400

    Get requests:

---
+++ src/components/showBlogs.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="show-blogs">
+    <h1>All Blog Articles</h1>
+    <div v-for="blog in blogs" class="single-blog">
+      {{ blog.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+  data() {
+    return {
+      blogs: []
+    }
+  },
+  methods: {
+
+  },
+
+  // fired when component is created
+  created() {
+    this.$http.get('https://jsonplaceholder.typicode.com/posts').then((data) => {
+      console.log(data);
+      this.blogs = data.body;
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
