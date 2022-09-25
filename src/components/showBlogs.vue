f6ba2ea1202a12f79f896da185ecb4c2627dbca0
Joe Lee <jdlee6@buffalo.edu>
2022-09-25 19:13:54 -0400

    Filters

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -1,15 +1,16 @@
 <template>
-  <!-- directives & arguments -->
   <div v-theme:column="'narrow'" id="show-blogs">
     <h1>All Blog Articles</h1>
     <div v-for="blog in blogs" class="single-blog">
-      <h2 v-rainbow>{{ blog.title }}</h2>
-      <article>{{ blog.body }}</article>
+      <h2>{{ blog.title | to-uppercase }}</h2>
+      <article>{{ blog.body | snippet }}</article>
     </div>
   </div>
 </template>
 
 <script>
+// Filters example:
+// Capitalize each text output in the html
 
 export default {
   components: {
